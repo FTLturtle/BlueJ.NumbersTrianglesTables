3e0cf4b0e154dd9d947e356e1735cdfb7e0ba742
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-06 08:45:07 -0500

    Completed all method stubs in NumberUtilities per the README-NumberUtilities instructions

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -1,32 +1,64 @@
 
-
+import java.lang.Math;
 
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String range = "";
+        
+        for (int n = 0; n < stop; n++){
+            range = range + n;
+        }
+        
+        return range;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String range = "";
+        
+        for (int n = start; n < stop; n++){
+            range = range + n;
+        }
+        
+        return range;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String range = "";
+        
+        for (int n = start; n < stop; n = n + step){
+            range = range + n;
+        }
+        
+        return range;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        if (start % 2 == 1){
+            start++;
+        }
+        
+        return getRange(start, stop, 2);
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        if (start % 2 == 0){
+            start++;
+        }
+        
+        return getRange(start, stop, 2);
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String expoRange = "";
+        
+        for (int n = start; n <= stop; n++){
+            expoRange = expoRange + Math.round(Math.pow(n, exponent));
+        }
+        
+        return expoRange;
     }
 }
