2bac7f65a8b4a911075a2e6e6beb653545c5a2a2
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-06 11:58:54 -0500

    Completed all method stubs in TriangleUtilities per the instructions in README-TriangleUtilites

--- TriangleUtilities.java
+++ TriangleUtilities.java
@@ -3,19 +3,37 @@
 public class TriangleUtilities {
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String row = "";
+        
+        for (int n = 0; n < numberOfStars; n ++){
+            row = row + "*";
+        }
+        
+        return row;
     }
     
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String triangle = "";
+        int rowNumber = 1;
+        
+        while (rowNumber <= numberOfRows){
+            for (int x = 0; x < rowNumber; x++){
+                triangle = triangle + "*";
+            }
+            
+            rowNumber++;
+            triangle = triangle + "\n";
+        }
+        
+        return triangle;
     }
 
 
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(4);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(9);
     }
 }
