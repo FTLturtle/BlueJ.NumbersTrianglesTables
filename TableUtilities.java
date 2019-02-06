f6b4555229c1f42a75bd0f15f0a20cf659db6972
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-06 12:42:08 -0500

    Filled in the method stubs in TableUtilities as per the instructions in README-TableUtilities

--- TableUtilities.java
+++ TableUtilities.java
@@ -2,14 +2,31 @@
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String multiplicationTable = "";
+        int rowCounter = 1;
+        int columnCounter = 1;
+        
+        while (columnCounter <= tableSize){
+            rowCounter = 1;
+            
+            while (rowCounter <= tableSize){
+                multiplicationTable = multiplicationTable + String.format("%3d", rowCounter * columnCounter) + " |";
+                rowCounter++;
+            }
+            
+            columnCounter++;
+            
+            multiplicationTable = multiplicationTable + "\n";
+        }
+        System.out.print(multiplicationTable);
+        return multiplicationTable;
     }
 }
